d211f7ff2b775d9e3c1d44d3e0c75520987dab7f
shufo <meikyowise@gmail.com>
2017-12-02 14:47:31 +0900

    Use attila as pelican theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-THEME = './pelican-themes/pelican-mg'
+THEME = './pelican-themes/attila'
 PYGMENTS_STYLE = 'solarizedlight'
 MD_EXTENSIONS = ['linkify', 'del_ins', 'fenced_code', 'codehilite(css_class=highlight)', 'tables']
 SHARE = True
 TAG_SAVE_AS = ''
+AUTHOR = 'shufo'
 AUTHOR_SAVE_AS = ''
-DIRECT_TEMPLATES = ('index', 'categories', 'archives', 'search', 'tipue_search')
+DIRECT_TEMPLATES = ('index', 'categories', 'archives')
 TIPUE_SEARCH_SAVE_AS = 'tipue_search.json'
 TWITTER_USERNAME = 'shufo_'
 DISQUS_SITENAME = 'shufo'
@@ -49,11 +50,11 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/shufo_'),
-         ('github', 'https://github.com/shufo'),
+SOCIAL = (('Twitter', 'https://twitter.com/shufo_'),
+         ('Github', 'https://github.com/shufo'),
          ('envelope', 'mailto:meikyowise@gmail.com'),)
 
-FOOTER = ("&copy; 2015 shufo. All rights reserved.<br>" +
+FOOTER = ("&copy; 2017 shufo. All rights reserved.<br>" +
               "Code snippets in the pages are released under " +
               "<a href=\"http://opensource.org/licenses/MIT\" target=\"_blank\">" +
               "The MIT License</a>, unless otherwise specified.")
@@ -79,3 +80,44 @@ SITEMAP = {
     }
 }
 
+HEADER_COVER = 'assets/header.jpg'
+
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+ARTICLE_URL = '{slug}.html'
+ARTICLE_SAVE_AS = '{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+MENUITEMS = (
+	('About', '/hello-world.html'),
+    ('Categories', '/categories'),
+	('GitHub', 'https://github.com/shufo')
+)
+
+AUTHORS_BIO = {
+  "shufo": {
+    "name": "shufo",
+    "cover": "https://avatars1.githubusercontent.com/u/1641039?s=400&u=e76a7e80d95805075226c95b7fc576ac0df4b6f1&v=4",
+    "image": "https://avatars1.githubusercontent.com/u/1641039?s=400&u=e76a7e80d95805075226c95b7fc576ac0df4b6f1&v=4",
+"website": "http://github.com/shufo",
+    "location": "Tokyo"
+  }
+}
+
+STATIC_PATHS = ['assets']
