102c5717d849d8d394a9c55e835f7bf2a17cc7c4
shufo <meikyowise@gmail.com>
2020-10-29 09:35:55 +0900

    feat: 🎸 add favicons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,13 @@ const autonavOptions = {
 module.exports = {
   title: "shufo.dev",
   description: "random automation stuff",
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/img/icons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/img/icons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/img/icons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/assets/img/icons/site.webmanifest"}],
+    ['link', { rel: "shortcut icon", href: "/assets/img/icons/favicon.ico"}],
+  ],
   lang: "ja",
   plugins: [
     ["autometa", autometaOptions],
@@ -81,7 +88,7 @@ module.exports = {
       {
         port: 9000
       }
-    ]
+    ],
   ],
   theme: "@vuepress/theme-blog", // OR shortcut: @vuepress/blog
   locales: {
@@ -120,6 +127,10 @@ module.exports = {
       {
         text: "Tags",
         link: "/tag/"
+      },
+      {
+        text: "Feed",
+        link: "https://shufo.dev/feed.atom"
       }
     ],
     /**
