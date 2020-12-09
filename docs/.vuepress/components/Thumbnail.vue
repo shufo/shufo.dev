9c8a028de1ad4a44723dfb04600e46b038dd3b35
shufo <meikyowise@gmail.com>
2020-12-10 02:31:15 +0900

    feat: 🎸 display skeleton while image loading

--- docs/.vuepress/components/Thumbnail.vue
+++ docs/.vuepress/components/Thumbnail.vue
@@ -1,8 +1,32 @@
 <template>
-  <!-- <div> -->
-  <img :src="src" />
-  <!-- </div> -->
+  <div>
+    <img v-show="isLoaded" :src="src" @load="onImgLoad" />
+    <Skeleton v-if="!isLoaded" height="300px" />
+  </div>
 </template>
+<script>
+import { Skeleton } from "vue-loading-skeleton";
+
+export default {
+  components: { Skeleton },
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isLoaded: false,
+    };
+  },
+  methods: {
+    onImgLoad() {
+      this.isLoaded = true;
+    },
+  },
+};
+</script>
 <style>
 .content-wrapper img {
   max-width: 100%;
