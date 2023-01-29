d71c4aafecc9e0658f97a48c087d4390b63dcd07
Shuhei Hayashibara <meikyowise@gmail.com>
2023-01-29 14:17:24 +0900

    refactor: 💡 delete unnecessary comment

--- .tina/config.ts
+++ .tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINA_CLIENT_ID, // Get this from tina.io
-  token: process.env.TINA_TOKEN, // Get this from tina.io
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
   build: {
     outputFolder: "admin",
     publicFolder: "public",
