dc6e5498554ff3c0bb5245e1a5e2f7fbdd285e6c
Shuhei Hayashibara <meikyowise@gmail.com>
2023-02-04 19:19:25 +0900

    feat: 🎸 add share button to post pages

---
+++ src/components/SocialButtons.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import {
+    LinkedIn,
+    Facebook,
+    Twitter,
+    Line,
+  } from "svelte-share-buttons-component";
+  import { SITE } from "@config";
+
+  export let title: string;
+  export let url: string;
+
+  const siteTitle = SITE.title.replace(/\./, "\\.");
+</script>
+
+<Twitter class="share-button" text="{title} - {siteTitle}" {url} via="shufo_" />
+<Facebook class="share-button" quote="{title} - {siteTitle}" {url} />
+<Line class="share-button" {url} />
+<LinkedIn class="share-button" {url} />
+
+<style>
+  .share-button {
+    padding: 0.5em;
+  }
+</style>
